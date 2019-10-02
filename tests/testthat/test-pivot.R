adc6afd315489e06340e2e1d918967c5b1b8084e
Edgar Ruiz <edgar@rstudio.com>
2019-10-02 10:40:11 -0500

    Increase test coverage

--- tests/testthat/test-pivot.R
+++ tests/testthat/test-pivot.R
@@ -4,6 +4,7 @@ test_that("pivot", {
   t <- mtcars %>%
     columns(am) %>%
     rows(cyl) %>%
-    values(mean(mpg))
+    values(mean(mpg)) %>%
+    focus(cyl == 6)
   expect_is(pivot(t), "pivot_table")
 })
